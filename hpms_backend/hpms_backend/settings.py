4c63652e58103d6b59be05f7510f7da8b81b0832
Abemelek Mesfin <abemelekmesfin07@gmail.com>
2026-07-15 18:31:55 +0300

    Last deploy 10

--- hpms_backend/hpms_backend/settings.py
+++ hpms_backend/hpms_backend/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'hpms_backend.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://hospitalpatientsmanagementsystem.netlify.app/"
+    "https://hospitalpatientsmanagementsystem.netlify.app"
 ]
 CORS_ALLOW_METHODS = [
     "DELETE",
